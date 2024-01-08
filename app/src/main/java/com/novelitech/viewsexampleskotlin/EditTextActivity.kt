6f5254cb796cfecf3bbafa4d3ea485da51c20a61
Bruno Noveli <brunimzuppwolf@gmail.com>
2024-01-08 15:31:41 -0300

    EditText example

---
+++ app/src/main/java/com/novelitech/viewsexampleskotlin/EditTextActivity.kt
@@ -0,0 +1,11 @@
+package com.novelitech.viewsexampleskotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditTextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_text)
+    }
+}
